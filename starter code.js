9c8ce6de3ab910e77b5d2bdafd53c0bfc7ea9eab
Kevin Faucher <126988757+kevinjfaucher@users.noreply.github.com>
2024-08-28 16:31:04 -0400

    Create starter code.js
    Initial Commit

---
+++ starter code.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//arrow function
+const randomDamage = () => {};
+
+//arrow function
+const chooseOption = (opt1, opt2) => {};
+
+//function expression
+const attackPlayer = function(health) {};
+
+//arrow function
+const logHealth = (player, health) => {};
+
+//arrow function
+const logDeath = (winner, loser) => {};
+
+//arrow function
+const isDead = health => {};
+
+//function declaration
+function fight(player1, player2, player1Health, player2Health) {}
+
+fight("Summer", "Winter", 40, 40);
